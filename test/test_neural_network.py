e5dd2aa0d124dcbc2256a69e02f3774b0294e77d
leesuckgeun <leesuckgeun@gmail.com>
2017-01-28 23:33:04 +0900

    test: adding hidden node test implemented

--- test/test_neural_network.py
+++ test/test_neural_network.py
@@ -90,6 +90,69 @@ class NeuralNetworkTest(unittest.TestCase):
         self.assertEqual(nn.connect_genes[5, 3], 1, "enabled")
         self.assertEqual(nn.connect_genes[5, 4], 5, "innovation number")
 
+    def test_add_hidden_node(self):
+        nn = NeuralNetwork(2, 1)
+        self.assertTrue(np.array_equal(nn.node_genes, np.array([0, 0, 1])))
+
+        # connection_gene
+        # in, out, weight, enabled, innov
+        #  0,   2,     w1,       1,     0, (connection_num = 0)
+        #  1,   2,     w2,       1,     1, (connection_num = 1)
+
+        # if connection number is given, add a hidden node there
+        # if not, add a hidden node to a random connection.
+        nn.add_hidden_node(connection_num=0)
+        self.assertTrue(np.array_equal(nn.node_genes, np.array([0, 0, 1, 2])))
+        # in, out, weight, enabled, innov
+        #  0,   2,     w1,       0,     0, (connection_num = 0)
+        #  1,   2,     w2,       1,     1, (connection_num = 1)
+        #  0,   3,      1,       1,     2, (connection_num = 2)
+        #  3,   2,     w1,       1,     3, (connection_num = 3)
+        self.assertEqual(nn.connect_genes[0, 3], 0, "previous connection disabled")
+
+        self.assertEqual(nn.connect_genes[2, 0], 0, "new connection input")
+        self.assertEqual(nn.connect_genes[2, 1], 3, "new connection out to hidden")
+        self.assertEqual(nn.connect_genes[2, 2], 1, "new connection front weight always one")
+        self.assertEqual(nn.connect_genes[2, 3], 1, "enabled")
+        self.assertEqual(nn.connect_genes[2, 4], 2, "Innovation number")
+
+        self.assertEqual(nn.connect_genes[3, 0], 3, "new connection hidden as input")
+        self.assertEqual(nn.connect_genes[3, 1], 2, "new connection output")
+        self.assertEqual(nn.connect_genes[3, 2], nn.connect_genes[0, 2], "assign previous weight")
+        self.assertEqual(nn.connect_genes[3, 3], 1, "enabled")
+        self.assertEqual(nn.connect_genes[3, 4], 3, "Innovation number")
+
+        # assign new value to weights
+        nn.connect_genes[2, 3] = 0.99
+        nn.connect_genes[3, 3] = -0.99
+        # in, out, weight, enabled, innov
+        #  0,   2,     w1,       0,     0, (connection_num = 0)
+        #  1,   2,     w2,       1,     1, (connection_num = 1)
+        #  0,   3,   0.99,       1,     2, (connection_num = 2)
+        #  3,   2,  -0.99,       1,     3, (connection_num = 3)
+        nn.add_hidden_node(connection_num=2)
+        self.assertTrue(np.array_equal(nn.node_genes, np.array([0, 0, 1, 2, 2])))
+        # in, out, weight, enabled, innov
+        #  0,   2,     w1,       0,     0, (connection_num = 0)
+        #  1,   2,     w2,       1,     1, (connection_num = 1)
+        #  0,   3,   0.99,       0,     2, (connection_num = 2)
+        #  3,   2,  -0.99,       1,     3, (connection_num = 3)
+        #  0,   4,      1,       1,     4, (connection_num = 4)
+        #  4,   3,   0.99,       1,     5, (connection_num = 5)
+        self.assertEqual(nn.connect_genes[2, 3], 0, "previous connection disabled")
+
+        self.assertEqual(nn.connect_genes[4, 0], 0, "new connection input")
+        self.assertEqual(nn.connect_genes[4, 1], 4, "new connection out to hidden")
+        self.assertEqual(nn.connect_genes[4, 2], 1, "new connection front weight always one")
+        self.assertEqual(nn.connect_genes[4, 3], 1, "enabled")
+        self.assertEqual(nn.connect_genes[4, 4], 4, "Innovation number")
+
+        self.assertEqual(nn.connect_genes[5, 0], 4, "new connection hidden as input")
+        self.assertEqual(nn.connect_genes[5, 1], 3, "new connection output")
+        self.assertEqual(nn.connect_genes[5, 2], nn.connect_genes[2, 2], "assign previous weight")
+        self.assertEqual(nn.connect_genes[5, 3], 1, "enabled")
+        self.assertEqual(nn.connect_genes[5, 4], 5, "Innovation number")
+
 
 def main():
     unittest.main()
