8f3c70eb284c76ffceb22505bde02bf5b0cc1b3d
leesuckgeun <leesuckgeun@gmail.com>
2017-01-31 23:15:42 +0900

    feat: workplace implemented

---
+++ globaladmin/workplace.py
@@ -0,0 +1,10 @@
+class Workplace:
+
+    def __init__(self, n_input, n_output):
+        self.n_input = n_input
+        self.n_output = n_output
+        self.innov_counter = -1
+        self.innov_history = []
+
+
+
