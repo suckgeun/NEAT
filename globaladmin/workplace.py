96e9896582315eeb88e48b7ce6ff605bc4cf56eb
leesuckgeun <leesuckgeun@gmail.com>
2017-02-05 13:50:36 +0900

    feat: workplace parameters added

--- globaladmin/workplace.py
+++ globaladmin/workplace.py
@@ -1,10 +1,16 @@
 class Workplace:
+    """
+    innov_history is dictionary with key:(in, out), value:counter
+    """
 
-    def __init__(self, n_input, n_output):
+    def __init__(self, n_input, n_output, n_nn=1, bias=1):
         self.n_input = n_input
         self.n_output = n_output
         self.innov_counter = -1
-        self.innov_history = []
+        self.innov_history = {}
+        self.n_nn = n_nn
+        self.nns = []
+        self.bias=bias
 
 
 
