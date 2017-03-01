0107237f0beea7c82093c4c827db043c9975b785
leesuckgeun <leesuckgeun@gmail.com>
2017-03-02 00:34:14 +0900

    feat: neural network outputs toggling implemented

--- players/neuralnet.py
+++ players/neuralnet.py
@@ -6,9 +6,49 @@ class NeuralNetwork:
     def __init__(self):
         self.node_genes = None
         self.connect_genes = None
-        self.outputs_prev = None
-        self.outputs_cur = None
         self.fitness = None
+        self._outputs1 = None
+        self._outputs2 = None
+        self._front = False
+
+    @property
+    def outputs_prev(self):
+        if self._front:
+            return self._outputs1
+        else:
+            return self._outputs2
+
+    @outputs_prev.setter
+    def outputs_prev(self, val):
+        assert type(val) is list, "val must be list"
+
+        if self._front:
+            self._outputs1 = val
+        else:
+            self._outputs2 = val
+
+    @property
+    def outputs_cur(self):
+        if self._front:
+            return self._outputs2
+        else:
+            return self._outputs1
+
+    def toggle(self):
+        self._front = not self._front
+
+    @outputs_cur.setter
+    def outputs_cur(self, val):
+        assert type(val) is list, "val must be list"
+
+        if self._front:
+            self._outputs2 = val
+        else:
+            self._outputs1 = val
+
+
+
+
 
 
 
